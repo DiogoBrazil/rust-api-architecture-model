765de5c8e3f269e85ba312ac54b6259c5eda73df
DiogoBrazil <diogoifroads@gmail.com>
2025-07-02 15:12:04 -0400

    Add configuration struct for environment variables

---
+++ src/config/config_env.rs
@@ -0,0 +1,18 @@
+use std::env;
+
+#[derive(Clone)]
+pub struct Config {
+    pub database_url: String,
+    pub server_addr: String,
+    pub jwt_secret: String,
+}
+
+impl Config {
+    pub fn from_env() -> Self {
+        Self {
+            database_url: env::var("DATABASE_URL").expect("DATABASE_URL must be set"),
+            server_addr: env::var("SERVER_ADDR").expect("SERVER_ADDR must be set"),
+            jwt_secret: env::var("JWT_SECRET").expect("JWT_SECRET must be set"),
+        }
+    }
+}
